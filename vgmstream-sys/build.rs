5e8ddb5c5a61b1490ee55faafc02e8469b676fcd
cohaereo <cohaereo@protonmail.com>
2023-12-10 20:04:50 +0100

    Temporarily disable CMake compilation, providing binaries for now

--- vgmstream-sys/build.rs
+++ vgmstream-sys/build.rs
@@ -3,31 +3,31 @@ const PROFILE: &'static str = "RelWithDebInfo";
 fn main() {
     // let out_dir = std::path::PathBuf::from(std::env::var("OUT_DIR").unwrap());
 
-    let dst = cmake::Config::new("vgmstream")
-        .define("BUILD_CLI", "OFF")
-        .define("BUILD_FB2K", "OFF")
-        .define("BUILD_WINAMP", "OFF")
-        .define("BUILD_XMPLAY", "OFF")
-        .define("BUILD_STATIC", "ON")
-        .define("USE_MPEG", "OFF")
-        .define("USE_VORBIS", "ON")
-        .define("USE_G719", "OFF")
-        .define("USE_FFMPEG", "OFF")
-        .define("USE_ATRAC9", "OFF")
-        .define("USE_CELT", "OFF")
-        .define("USE_SPEEX", "OFF")
-        .define("USE_G7221", "OFF")
-        .define("USE_G719", "OFF")
-        .build_target("libvgmstream")
-        .profile(PROFILE)
-        // // cohae: Kind of disappointing that the cmake crate doesnt handle this on it's own
-        // .define("CMAKE_LIBRARY_OUTPUT_DIRECTORY", &out_dir)
-        .build();
+    // let dst = cmake::Config::new("vgmstream")
+    //     .define("BUILD_CLI", "OFF")
+    //     .define("BUILD_FB2K", "OFF")
+    //     .define("BUILD_WINAMP", "OFF")
+    //     .define("BUILD_XMPLAY", "OFF")
+    //     .define("BUILD_STATIC", "ON")
+    //     .define("USE_MPEG", "OFF")
+    //     .define("USE_VORBIS", "ON")
+    //     .define("USE_G719", "OFF")
+    //     .define("USE_FFMPEG", "OFF")
+    //     .define("USE_ATRAC9", "OFF")
+    //     .define("USE_CELT", "OFF")
+    //     .define("USE_SPEEX", "OFF")
+    //     .define("USE_G7221", "OFF")
+    //     .define("USE_G719", "OFF")
+    //     .build_target("libvgmstream")
+    //     .profile(PROFILE)
+    //     // // cohae: Kind of disappointing that the cmake crate doesnt handle this on it's own
+    //     // .define("CMAKE_LIBRARY_OUTPUT_DIRECTORY", &out_dir)
+    //     .build();
 
-    println!(
-        "cargo:rustc-link-search=native={}",
-        dst.join("build/src/").join(PROFILE).display()
-    );
+    // println!(
+    //     "cargo:rustc-link-search=native={}",
+    //     dst.join("build/src/").join(PROFILE).display()
+    // );
 
     let project_path = std::path::PathBuf::from(std::env::var("CARGO_MANIFEST_DIR").unwrap());
     println!(
